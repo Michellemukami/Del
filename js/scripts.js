a009213452dd4ae59d90a4652457cfb0fcaf57c0
Michelle Mukami <you@example.com>
2019-06-10 08:18:54 +0300

    changes made in js

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$('.main').click(function() {
+  $(this).children('.hide').slideToggle(1200);
+  $(this).children('.show').toggle(1500);
+});
+$("button").click(function(kami) {
+  var client = document.getElementById('client').value;
+  alert(client + ' we have received your message' + 'Thank you');
+  kami.preventDefault();
+});
+$("button").on('click', function() {
+  $('form').each(function() {
+    this.reset();
+  });
+});
